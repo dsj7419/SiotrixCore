36490c95f845591545b1d65d9d8816353fcc64ee
Dan Johnson <dsj7419@gmail.com>
2017-08-23 21:52:04 -0700

    updated filter to add warning points to each word for guilds

--- src/Siotrix.Core/Entities/Modules/Moderation/DiscordGuildFilterList.cs
+++ src/Siotrix.Core/Entities/Modules/Moderation/DiscordGuildFilterList.cs
@@ -9,15 +9,22 @@ namespace Siotrix
         [Required]
         public long GuildId { get; private set; }
         [Required]
-        public string Word{ get; private set; }
+        public string Word { get; private set; }
+        [Required]
+        public int WarnPoints { get; private set; }
 
         public DiscordGuildFilterList() { }
 
-        public DiscordGuildFilterList(long guildId, string word)
+        public DiscordGuildFilterList(long guildId, string word, int warnPoints)
         {
             GuildId = guildId;
             Word = word;
+            WarnPoints = warnPoints;
         }
 
+        public void SetWarnPoints(int warnPoints)
+        {
+            WarnPoints = warnPoints;
+        }
     }
 }
